6c7954a970f8aaa5eb7ca1819730c08b20393cc3
Monojit Datta <datta.monojit@gmail.com>
2021-02-01 19:31:01 +0530

    appgw

--- Setup/AppGW/aks-lt-appgw-deploy.ps1
+++ Setup/AppGW/aks-lt-appgw-deploy.ps1
@@ -1,22 +1,25 @@
-param([Parameter(Mandatory=$true)] [string] $rg,
-        [Parameter(Mandatory=$true)] [string] $fpath,
-        [Parameter(Mandatory=$true)] [string] $deployFileName,
-        [Parameter(Mandatory=$true)] [string] $appgwName,
-        [Parameter(Mandatory=$true)] [string] $vnetName,
-        [Parameter(Mandatory=$true)] [string] $subnetName,
-        [Parameter(Mandatory=$true)] [string] $hostName,
-        [Parameter(Mandatory=$true)] [string] $backendIPAddress)
+param([Parameter(Mandatory=$false)] [string] $rg = "jmeter-workshop-rg",
+        [Parameter(Mandatory=$false)] [string] $fpath = "/Users/monojitd/Materials/Projects/AKSProjects/LoadTests/NginxCacheLoadTest/Setup",
+[Parameter(Mandatory=$false)] [string] $deployFileName = "aks-lt-appgw-deploy", 
+        [Parameter(Mandatory=$false)] [string] $appgwName = "aks-lt-appgw",
+        [Parameter(Mandatory=$false)] [string] $vnetName = "aks-lt-vnet",
+        [Parameter(Mandatory=$false)] [string] $subnetName = "aks-lt-appgw-subnet",
+        [Parameter(Mandatory=$false)] [string] $backendIPAddress = "187.0.0.200",
+        [Parameter(Mandatory=$false)] [string] $backendPoolHostName = "internal.wkshpdev.com",
+        [Parameter(Mandatory=$false)] [string] $listenerHostName = "wkshpdev.com")
 
 Test-AzResourceGroupDeployment -ResourceGroupName $rg `
 -TemplateFile "$fpath/AppGW/$deployFileName.json" `
+-TemplateParameterFile "$fpath/AppGW/$deployFileName.parameters.json" `
 -applicationGatewayName $appgwName `
 -vnetName $vnetName -subnetName $subnetName `
--hostName $hostName `
--backendIpAddress1 $backendIPAddress
+-backendPoolHostName $backendPoolHostName `
+-listenerHostName $listenerHostName
 
 New-AzResourceGroupDeployment -ResourceGroupName $rg `
 -TemplateFile "$fpath/AppGW/$deployFileName.json" `
+-TemplateParameterFile "$fpath/AppGW/$deployFileName.parameters.json" `
 -applicationGatewayName $appgwName `
 -vnetName $vnetName -subnetName $subnetName `
--hostName $hostName `
--backendIpAddress1 $backendIPAddress
+-backendPoolHostName $backendPoolHostName `
+-listenerHostName $listenerHostName
